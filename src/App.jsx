1fc412998b8e552c782586aff1dbf54ca9744651
Aman Singh <thakuraman8630@gmail.com>
2023-10-14 10:21:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
+function App() {
+ 
+
+  return (
+    <>
+      {/* <Header/>
+      <Home/>
+      <Footer/> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github () {
+    const data=useLoaderData()
+    // const [data,setData]=useState([])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/AmanSingh2427')
+    //     .then(response=>response.json())
+    //     .then(data=>{
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // })
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github follower : {data.followers}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  )
+}
+export default Github
+
+export const githubInfoLoader=async()=>{
+    const response=await fetch('https://api.github.com/users/AmanSingh2427')
+    return response.json()
+}
